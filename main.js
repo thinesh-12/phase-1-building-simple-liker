94d025bf95bba69a49fabc510237bb736cf8dd4f
THINESHKUMAR <thinesh12@gmal.com>
2023-04-30 08:40:14 -0400

    edit{

--- main.js
+++ main.js
@@ -4,14 +4,29 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 
+const heart = document.querySelector('.like-glyph');
+const errorModal = document.querySelector('#modal');
+const errorModalMessage = document.querySelector('#modal-message');
 
+// Hide the error modal when the page loads
+errorModal.classList.add('hidden');
+
+heart.addEventListener('click', () => {
+  // Change heart to full or empty immediately
+  if (heart.innerHTML === EMPTY_HEART) {
+    heart.innerHTML = FULL_HEART;
+    heart.classList.add('activated-heart');
+  } else {
+    heart.innerHTML = EMPTY_HEART;
+    heart.classList.remove('activated-heart');
+  }
 
 
 //------------------------------------------------------------------------------
 // Don't change the code below: this function mocks the server response
 //------------------------------------------------------------------------------
 
-function mimicServerCall(url="http://mimicServer.example.com", config={}) {
+function mimicServerCall() {
   return new Promise(function(resolve, reject) {
     setTimeout(function() {
       let isRandomFailure = Math.random() < .2
@@ -20,6 +35,7 @@ function mimicServerCall(url="http://mimicServer.example.com", config={}) {
       } else {
         resolve("Pretend remote server notified of action!");
       }
-    }, 300);
+    }, 300);   
   });
 }
+});
